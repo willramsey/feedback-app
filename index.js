5dedba0dadda1405f7c0ade7b5d2552e3cf20947
William Ramsey <wramsey@gmail.com>
2017-09-22 12:44:14 -0700

    Fix typo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.hmtl'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
